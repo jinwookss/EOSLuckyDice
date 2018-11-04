78bffb00601091a6e9bd998b53071e4addd6b1ea
Jinwook Jeong <jjw6215@gmail.com>
2018-11-04 22:47:27 +0900

    Update header and source file

--- include/dice.hpp
+++ include/dice.hpp
@@ -3,15 +3,62 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 
-class [[eosio::contract]] dice : public eosio::contract {
+#include <string>
+#include <list>
+
+using std::string;
+using std::list;
+using eosio::asset;
+using eosio::name;
+using eosio::contract;
+
+class [[eosio::contract]] dice : public contract {
 
 public:
+    dice(name s, name code, eosio::datastream<const char*> ds) : contract::contract(s, code, ds) {}
+
     [[eosio::action]]
     void bet(name user, asset number);
 
     [[eosio::action]]
-    void notify_result(name user, asset result, asset profit);
+    void notires(name user, asset result, asset profit);
 
     [[eosio::action]]
-    
+    void reguser(name user, string nick_name);
+
+    [[eosio::action]]
+    void dereguser(name user);
+
+    [[eosio::action]]
+    void upsertgame(name user, string date, asset number, asset result, asset profit);
+
+    [[eosio::action]]
+    void erasegame(name user);
+
+    [[eosio::action]]
+    void history(name user);
+
+private:
+    struct [[eosio::table]] person {
+        name key;
+        string nick_name;
+
+        uint64_t primary_key() const { return key.value; }
+    };
+    typedef eosio::multi_index<"users"_n, person> user_index;
+
+    struct game {
+        string date;
+        asset number;
+        asset result;
+        asset profit;
+    };
+
+    struct [[eosio::table]] game_info {
+        name key;
+        list<game> games;
+
+        uint64_t primary_key() const { return key.value; }
+    };
+    typedef eosio::multi_index<"games"_n, game_info> game_index;
 };
